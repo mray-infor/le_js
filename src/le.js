7350035a80cd67de4b39b01e2262e690fefbee2e
mray-infor <36543569+mray-infor@users.noreply.github.com>
2018-02-16 11:40:27 -0500

    the anonymous-module func does not work for us
    Causes both a compilation warning and a massive runtime error when used in our project

--- src/le.js
+++ src/le.js
@@ -8,12 +8,7 @@
 
 /** @param {Object} window */
 (function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous module.
-        define(function() {
-            return factory(root);
-        });
-    } else if (typeof exports === 'object') {
+    if (typeof exports === 'object') {
         // Node. Does not work with strict CommonJS, but
         // only CommonJS-like environments that support module.exports,
         // like Node.
